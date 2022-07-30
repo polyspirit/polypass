9ce22f02854251e229887ee90038331f63f3ebd3
polyspirit <polyspirit@DESKTOP-DTMI1M8.localdomain>
2022-07-30 16:37:32 +0400

    fix textarea

--- resources/views/parts/fields/textarea.blade.php
+++ resources/views/parts/fields/textarea.blade.php
@@ -7,9 +7,7 @@ $attrTitleValue = $disabled ? __('global.copy') . ' ' . $title : $title;
     <label for="textarea-{{ $name }}" class="form-label" data-copied="{{ __('global.copied') }}">{{ $title }}</label>
     <textarea name="{{ $name }}" id="textarea-{{ $name }}" rows="10"
         class="form-control @error($name) is-invalid @enderror" aria-describedby="{{ $name }}Help"
-        title="{{ $attrTitleValue }}" {{ $disabled }}>
-        {{ $value }}
-    </textarea>
+        title="{{ $attrTitleValue }}" {{ $disabled }}>{{ $value }}</textarea>
 </div>
 @error($name)
     <div id="{{ $name }}Help" class="form-text alert alert-danger">{{ $message }}</div>
