33d7a803b7a0aa4b02b140d32b714c60acb7c638
polyspirit <polyspirit@DESKTOP-DTMI1M8.localdomain>
2022-12-01 14:29:10 +0400

    env file public folder setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('path.public', function() {
+            return base_path(env('APP_PUBLIC_FOLDER', 'public'));
+        });
     }
 
     /**
